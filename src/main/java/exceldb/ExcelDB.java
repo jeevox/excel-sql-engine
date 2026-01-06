97bbc1040eaa9417a370c37695f0fc86d7f90fb9
jeevox <needforcodegvthan@gmail.com>
2026-01-06 20:02:50 +0530

    feat: add ExcelDB connection
    - Introduced ExcelDB as the primary entry point for the Excel SQL engine
    - Manages workbook lifecycle (connect, executeQuery, close)
    - Provides a Fillo-style API for interacting with Excel files

--- src/main/java/exceldb/ExcelDB.java
+++ src/main/java/exceldb/ExcelDB.java
@@ -1,1 +1,34 @@
-package exceldb; public class ExcelDB {}
+/**
+ * Author: Jeevithan Ambikapathy
+ * Description: SQL-like Excel database connection using Apache POI
+ */
+package exceldb;
+
+import org.apache.poi.ss.usermodel.*;
+import java.io.*;
+
+public class ExcelDB {
+    private Workbook workbook;
+    private String path;
+
+    private ExcelDB(String path) throws Exception {
+        this.path = path;
+        this.workbook = WorkbookFactory.create(new File(path));
+    }
+
+    public static ExcelDB connect(String path) throws Exception {
+        return new ExcelDB(path);
+    }
+
+    public ResultSet executeQuery(String sql) {
+        return ExcelExecutor.select(workbook, sql);
+    }
+
+    public void executeUpdate(String sql) throws Exception {
+        ExcelExecutor.update(workbook, path, sql);
+    }
+
+    public void close() throws Exception {
+        workbook.close();
+    }
+}
